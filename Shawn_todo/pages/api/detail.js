76831fcfcda5e8c45418c440359b0543b9391b88
sh805190 <34245021+sh805190@users.noreply.github.com>
2021-12-05 19:26:27 -0400

    Update detail.js

--- Shawn_todo/pages/api/detail.js
+++ Shawn_todo/pages/api/detail.js
@@ -1,3 +1,5 @@
+// Mock up API and Mock up Data
+
 const DATA = [{ id: 'todo-0', title: 'Math', createdDate: 'Sun Dec 05 2021 02:13:10 GMT-0400 (Atlantic Standard Time)', completedDate: null, 
 description: "Mathematics (from Greek: μάθημα, máthēma, 'knowledge, study, learning') includes the study of such topics as numbers (arithmetic and number theory),[1] formulas and related structures (algebra),[2] shapes and spaces in which they are contained (geometry),[1] and quantities and their changes (calculus and analysis).[3][4][5] There is no general consensus about its exact scope or epistemological status.[6][7]", completed: false ,uploadDate:"Sun Dec 05 2021 02:13:10 GMT-0400 (Atlantic Standard Time)"},
 { id: 'todo-1', title: 'Art', createdDate: 'Sun Dec 05 2021 02:13:10 GMT-0400 (Atlantic Standard Time)', completedDate: null, description: "Art is a wide range of human activities (or the products thereof) that involve creative imagination and an aim to express technical proficiency, beauty, emotional power, or conceptual ideas.", completed: false,uploadDate:"Sun Dec 05 2021 02:13:10 GMT-0400 (Atlantic Standard Time)" },
